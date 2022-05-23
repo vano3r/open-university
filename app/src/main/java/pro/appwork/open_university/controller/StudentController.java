c45eaf615302311257363624279f2ad9187e93c5
ivan.vorobev <ivan.vorobev@rt.ru>
2022-05-24 00:09:53 +0300

    Наладил ссылки для преподавателей

--- app/src/main/java/pro/appwork/open_university/controller/StudentController.java
+++ app/src/main/java/pro/appwork/open_university/controller/StudentController.java
@@ -2,62 +2,15 @@ package pro.appwork.open_university.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequiredArgsConstructor
-@RequestMapping("/students")
+@RequestMapping("/student")
 public class StudentController {
-//    private final MailService mailService;
-//    private final GroupService groupService;
-//    private final UserService userService;
-//    private final SolutionService solutionService;
-////    private final SubjectService subjectService;
-//
-//    @GetMapping()
-//    public String viewAddNewStudentPage(Model model) {
-//        model.addAttribute("groups", groupService.getAll());
-//        return "add-new-student-page";
-//    }
-//
-//    @PostMapping
-//    public String addNewStudent(@RequestParam String email, @RequestParam Long groupId, Model model) {
-//        mailService.send(email, null,null);
-//        model.addAttribute("groups", groupService.getAll());
-//        return "add-new-student-page";
-//    }
-//
-//    @GetMapping("/{studentId}/subjects")
-//    public String viewStudentSubjectPage(@PathVariable Long studentId, Model model) {
-//        CustomUser student = userService.getById(studentId);
-//        model.addAttribute("student", student);
-//
-//        return "student-subjects-page";
-//    }
-//
-//    @PostMapping("/{studentId}/subjects/{subjectId}/tasks/{taskId}")
-//    public String addSolution(@RequestParam MultipartFile file,
-//                              @PathVariable Long studentId,
-//                              @PathVariable Long subjectId,
-//                              @PathVariable Long taskId,
-//                              Model model) {
-//
-//        CustomUser student = userService.getById(studentId);
-//
-////        Subject subject = student.group().subjects().stream()
-////                .filter(sbj -> sbj.getId().equals(subjectId))
-////                .findFirst()
-////                .orElseThrow();
-////
-////        Task task = subject.getTasks().stream()
-////                .filter(t -> t.getId().equals(taskId))
-////                .findFirst()
-////                .orElseThrow();
-//
-//
-////        solutionService.createSolution(student, subject, task, file);
-//
-//        model.addAttribute("student", student);
-//        return "student-subjects-page";
-//    }
+    @GetMapping
+    public String viewStudentPage() {
+        return "layout/navbar-layout";
+    }
 }
