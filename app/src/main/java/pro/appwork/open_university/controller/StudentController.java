8f5db8b2e99152311a54d3dd138581db079d5fac
ivan.vorobev <ivan.vorobev@rt.ru>
2022-05-26 11:54:13 +0300

    fix

--- app/src/main/java/pro/appwork/open_university/controller/StudentController.java
+++ app/src/main/java/pro/appwork/open_university/controller/StudentController.java
@@ -1,11 +1,14 @@
 package pro.appwork.open_university.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pro.appwork.open_university.model.entity.Lesson;
+import pro.appwork.open_university.model.entity.Student;
+import pro.appwork.open_university.security.CustomUserDetails;
 import pro.appwork.open_university.service.CourseGroupService;
 import pro.appwork.open_university.service.GroupService;
 import pro.appwork.open_university.service.LessonService;
@@ -22,8 +25,10 @@ public class StudentController {
     private final LessonService lessonService;
 
     @GetMapping
-    public String viewStudentPage(Model model) {
-        Map<Integer, List<Integer>> listMap = courseGroupService.getAllByGroupId(1L);
+    public String viewStudentPage(Model model, Authentication authentication) {
+        Student student = (Student) ((CustomUserDetails) authentication.getPrincipal()).user();
+
+        Map<Integer, List<Integer>> listMap = courseGroupService.getAllByGroupId(student.getGroup().getId());
         listMap.keySet().forEach(
                 s -> listMap.get(s).forEach(System.out::println)
         );
@@ -31,7 +36,7 @@ public class StudentController {
 
         model.addAttribute("courses", listMap);
         model.addAttribute("lessons", lessons);
-        model.addAttribute("group", groupService.getById(1L));
+        model.addAttribute("group", groupService.getById(student.getGroup().getId()));
         return "student-page";
     }
 }
