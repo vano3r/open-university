4f68ac71950cb87c2c21a12b493492eb66f114b4
Ivan Vorobiev <ivan.v0r0biev@yandex.ru>
2022-05-30 19:43:10 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     java
     id("org.springframework.boot") version "2.6.6"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
+    id("maven-publish")
 }
 
 group = "pro.appwork"
@@ -9,6 +10,24 @@ version = "0.0.1"
 
 repositories {
     mavenCentral()
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/vano3r/open-university")
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
+    publications {
+        register<MavenPublication>("gpr") {
+            from(components["java"])
+        }
+    }
 }
 
 dependencies {
@@ -20,9 +39,13 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-validation")
-//    implementation("org.liquibase:liquibase-core")
     implementation("org.thymeleaf.extras:thymeleaf-extras-springsecurity5")
-    implementation("com.google.guava:guava:30.1.1-jre")
+
+//    implementation("com.google.guava:guava:30.1.1-jre")
+//    implementation("org.liquibase:liquibase-core")
+//    implementation("org.mapstruct:mapstruct:1.4.2.Final")
+//    annotationProcessor("org.projectlombok:lombok-mapstruct-binding:0.2.0")
+//    annotationProcessor("org.mapstruct:mapstruct-processor:1.4.2.Final")
 
     compileOnly("org.projectlombok:lombok")
     annotationProcessor("org.projectlombok:lombok")
@@ -32,3 +55,9 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.security:spring-security-test")
 }
+
+//kapt {
+//    arguments {
+//        arg("mapstruct.defaultComponentModel", "spring")
+//    }
+//}
